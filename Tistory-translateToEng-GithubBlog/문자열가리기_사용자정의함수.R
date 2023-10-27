131c409f1692b4e2db331e3ad9f5a60e18519d31
kkkjaahyun <jhdmbwy12@gmail.com>
2023-10-27 17:14:10 +0900

    first commit

---
+++ Tistory-translateToEng-GithubBlog/문자열가리기_사용자정의함수.R
@@ -0,0 +1,9 @@
+# 문자열 가리기(문자열길이와 동일한 길이의 '*'반복 만들기
+hideString = function(string){
+  nChar=nchar(string)
+  V = rep(c('*'),nChar)
+  repStar = paste(V,collapse = "")
+  print(repStar)
+}
+
+hideString('8575420e51135469ef68b37d1bdc4634_56039822660c5e01acbce811a10d4f28')
